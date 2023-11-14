90cdec25d4fa52ef637ce68d40307933a59b0692
vitzkrieg <vitzkrieg@gmail.com>
2023-11-14 07:03:01 -0500

    tribe events override components

---
+++ tribe/events/blocks/event-price.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Block: Event Price
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/blocks/event-price.php
+ *
+ * See more documentation about our Blocks Editor templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 4.7
+ *
+ */
+
+$cost             = $this->attr( 'cost' );
+$cost_description = $this->attr( 'costDescription' );
+$cost_title		  = get_theme_mod( 'inharmony_event_cost_title', 'Cost:' );
+?>
+<div class="tribe-block tribe-block__event-price">
+	<?php if ( $cost ) : ?>
+		<span class="tribe-block__event-price__title"> <?php echo esc_html( $cost_title ); ?> </span>
+		<span class="tribe-block__event-price__cost"> <?php echo esc_html( $cost ); ?> </span>
+	<?php endif; ?>
+	<?php if ( $cost_description ) : ?>
+		<span class="tribe-block__event-price__description"><?php echo esc_html( $cost_description ); ?></span>
+	<?php endif ?>
+</div>
