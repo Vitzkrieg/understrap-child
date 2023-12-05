8215413c783b6e8aff2849423841640a6d62e987
vitzkrieg <vitzkrieg@gmail.com>
2023-12-05 01:19:24 -0500

    move php shim function to helpers.php

--- inc/helpers.php
+++ inc/helpers.php
@@ -69,3 +69,23 @@ function inharmony_get_bs_css_from_theme_mod( string $value, string $size = '' )
     return $css;
 
 }
+
+
+
+if (!function_exists('str_contains')) {
+    function str_contains(string $haystack, string $needle) : bool {
+        return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+    }
+}
+
+if ( !function_exists( 'str_starts_with' ) ) {
+	function str_starts_with(string $haystack, string $needle): bool {
+		return \strncmp($haystack, $needle, \strlen($needle)) === 0;
+	}
+}
+
+if ( !function_exists( 'str_ends_with' ) ) {
+	function str_ends_with(string $haystack, string $needle): bool {
+		return $needle === '' || $needle === \substr($haystack, - \strlen($needle));
+	}
+}
