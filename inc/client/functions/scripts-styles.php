d30b4864374522c1531eec48533c06ef4aac272d
Vitzkrieg <dustin@vitzkrieg.net>
2025-07-18 05:45:10 -0400

    dequeque wc styles

--- inc/client/functions/scripts-styles.php
+++ inc/client/functions/scripts-styles.php
@@ -23,6 +23,10 @@ define( 'IHK_CSS_DIR', IHK_URL . '/dist/css' );
 
 function ihk_enqueue_scripts_styles()  
 {
+	// dequeue styles
+	wp_dequeue_style( 'woocommerce-general' );
+	wp_dequeue_style( 'woocommerce-layout' );
+	wp_dequeue_style( 'woocommerce-smallscreen' );
 
 	/* ------------------------------------------------------------------------ */
 	/* Register Scripts */
